b759e60aab942145adf4b256722b32010046e04c
João Rangel <joaohcrangel@gmail.com>
2018-11-20 13:12:29 -0200

    👮 Add Content-Security-Policy

--- main.js
+++ main.js
@@ -98,7 +98,10 @@ function createWindowExternal () {
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 800, 
+    height: 600
+  })
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
